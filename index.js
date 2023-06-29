b9c62552aa6bf193d12c85b19cfd17f037e28990
Jerricke <eejerrickw@gmail.com>
2023-06-29 15:26:29 -0600

    complete

--- index.js
+++ index.js
@@ -1,4 +1,72 @@
 /* Your Code Here */
+function createEmployeeRecord(data) {
+    const employee = {
+        firstName: data[0],
+        familyName: data[1],
+        title: data[2],
+        payPerHour: data[3],
+        timeInEvents: [],
+        timeOutEvents: [],
+    }
+    return employee;
+}
+
+function createEmployeeRecords(data) {
+    const records = [];
+    data.forEach( employee => records.push(createEmployeeRecord(employee)))
+    return records
+}
+
+
+function createTimeInEvent(date) {
+    const timeInData = {};
+    const dateInfo = date.split(" ");
+    timeInData.type = "TimeIn";
+    timeInData.hour = parseInt(dateInfo[1]);
+    timeInData.date = dateInfo[0];
+    this.timeInEvents.push(timeInData);
+
+    return this
+}
+
+function createTimeOutEvent(date) {
+    const timeOutData = {};
+    const dateInfo = date.split(" ");
+    timeOutData.type = "TimeOut";
+    timeOutData.hour = parseInt(dateInfo[1]);
+    timeOutData.date = dateInfo[0];
+    this.timeOutEvents.push(timeOutData);
+
+    return this
+}
+
+function hoursWorkedOnDate(date) {
+    const dayIn = this.timeInEvents.find( dateInfo => dateInfo.date === date);
+    const dayOut = this.timeOutEvents.find( dateInfo => dateInfo.date === date);
+    const hoursWorked = (dayOut.hour - dayIn.hour)/100;
+    return hoursWorked
+}
+
+function wagesEarnedOnDate(date) {
+    const wage = hoursWorkedOnDate.call(this, date) * this.payPerHour;
+    return wage;
+}
+
+// function allWagesFor() {
+//     let wage = 0;
+//     record.timeInEvents.forEach( day => {
+//         wage += wagesEarnedOnDate(day.date);
+//     })
+//     return wage
+// }
+
+// function calculatePayroll(employees) {
+//     let payroll = 0;
+//     employees.forEach( employee => {
+//         payroll += allWagesFor(employee);
+//     })
+//     return payroll
+// }
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
@@ -21,3 +89,14 @@ const allWagesFor = function () {
     return payable
 }
 
+let findEmployeeByFirstName = function(srcArray, firstName) {
+    return srcArray.find(function(rec){
+      return rec.firstName === firstName
+    })
+  }
+  
+  let calculatePayroll = function(arrayOfEmployeeRecords){
+      return arrayOfEmployeeRecords.reduce(function(memo, rec){
+          return memo + allWagesFor.call(rec)
+      }, 0)
+  }
